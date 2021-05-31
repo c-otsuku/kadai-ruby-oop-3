5dae20c618fa6f61d8052e56be00c7bd80c5d8d3
chika.ootsuku <c-otsuku@rosca.jp>
2021-05-31 08:46:12 +0000

    kadai-ruby-oop-3

---
+++ animal.rb
@@ -0,0 +1,20 @@
+class Animal
+    attr_accessor :name, :age
+    
+    def initialize(name, age)
+        self.name = name
+        self.age = age
+    end
+    
+    def say
+        puts "#{self.name}です。#{self.age}歳です。"
+    end
+end
+
+# tanaka = Human.new("田中　太郎", 25)
+# suzuki = Human.new("鈴木　次郎", 30)
+# sato = Human.new("佐藤　花子", 20)
+
+# tanaka.say
+# suzuki.say
+# sato.say
